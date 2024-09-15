34a8e58f7430bc6f8cafbab778d8fdf9f4a379d0
Labores olis <122188954+laboressolis@users.noreply.github.com>
2024-09-15 23:03:19 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -89,7 +89,7 @@ particlesJS('particles-js',
           "mode": "repulse"
         },
         "onclick": {
-          "enable": false,
+          "enable": true,
           "mode": "push"
         },
         "resize": true
